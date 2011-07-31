3ef3212fe5328a01c6eb3f067748059c576eeb28
Ron De Benedetti <rdlandfill@gmail.com>
2011-07-30 21:45:28 -0500

    initialize rails application 'appname' for heroku.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marilyn::Application.config.secret_token = '2a0f1f57c2ed03e24ab19e141adbb10e2a04ccc5487e0b23b9175cdb6fb4d1b00648c18fa9a945cadfec4a3540c11b088146832068cc53c4f45553fa16825f1b'
