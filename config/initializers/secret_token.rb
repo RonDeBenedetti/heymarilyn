4e862470570849a87bedb7ae7bd15e873693e78c
Ron De Benedetti <rdlandfill@gmail.com>
2011-07-30 22:07:10 -0500

    fuck these comments

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marilyn::Application.config.secret_token = 'd93516142853ee65aaaa24fbe07d6ce7cffd40b83dd831b978be6158539286ffabb3e2e9c99651073ce88a7de296a1b6f95c8cfc17bfaa9b13e839e4b059685b'
